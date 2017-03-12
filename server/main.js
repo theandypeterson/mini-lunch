af2a22e855eb53771c8bf1db99afa2e31ec0a38d
thenandypeterson <andy.peterson@atomicobject.com>
2017-03-11 22:10:10 -0500

    Add server

---
+++ server/main.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const debug = require('debug')('app:server')
+const webpack = require('webpack')
+const webpackConfig = require('../build/webpack.config')
+const config = require('../config')
+
+const app = express()
+const bodyParser = require('body-parser')
+const paths = config.utils_paths
+
+var env = 'development'
+
+app.use(require('connect-history-api-fallback')())
+
+if (config.env === 'development') {
+  const compiler = webpack(webpackConfig)
+
+  debug('Enable webpack dev and HMR middleware')
+  app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath  : webpackConfig.output.publicPath,
+    contentBase : paths.client(),
+    hot         : true,
+    quiet       : config.compiler_quiet,
+    noInfo      : config.compiler_quiet,
+    lazy        : false,
+    stats       : config.compiler_stats
+  }))
+  app.use(require('webpack-hot-middleware')(compiler))
+  app.use(bodyParser.json())
+
+} else {
+  debug(
+    'Server is being run outside of live development mode, meaning it will ' +
+    'only serve the compiled application bundle in ~/dist. Generally you ' +
+    'do not need an application server for this and can instead use a web ' +
+    'server such as nginx to serve your static files. See the "deployment" ' +
+    'section in the README for more information on deployment strategies.'
+  )
+
+  app.use(express.static(paths.dist()))
+}
+
+module.exports = app;
